c4fd68634e149b4f6ba4cae86c50736606ccecad
Minchonang <84889516+Minchonang@users.noreply.github.com>
2021-08-17 18:00:11 +0900

    UpDownGame
    1 ~ 30 랜덤으로 정해진 숫자를 맞추는 게임입니다.

---
+++ CreditViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by 민초냉밥 on 2021/07/05.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+    
+    @IBAction func touchUpDismissButton(_ sender: UIButton){
+        dismiss(animated: true,
+                completion: nil)
+    }
+}
